6d3482d01c4b28a3c40693cb54e454ba1c159146
Vishal Kumar Maurya <vm7740014@gmail.com>
2023-01-20 21:16:17 +0530

    useSearchParams

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams,useSearchParams } from "react-router-dom";
 const UserDetails = () => {
     const {userId}=useParams();
     const [data, setData] = useState([]);
+    const [searchParams,setSearchParams]=useSearchParams();
+
+    console.log(searchParams.get(`name`));
+    // http://localhost:3000/1001?name=Vishal
+    const reset =()=>{
+        setSearchParams({})
+    }
     useEffect(() => {
         
       const getData = async () => {
@@ -18,11 +25,13 @@ const UserDetails = () => {
 console.log(data);
   return (
     <div>
-        <h3>{data[0].id}</h3>
+        {/* <h3>{data[0].id}</h3>
         <img src={data[0].imageUrl} alt="" />
         <h3>{data[0].email}</h3>
         <h3>{data[0].firstName}</h3>
-        <h3>{data[0].lastName}</h3>
+        <h3>{data[0].lastName}</h3> */}
+        <h2>{searchParams.get(`name`)}</h2>
+        <button onClick={()=>reset()}>reset</button>
  
     </div>
   )
